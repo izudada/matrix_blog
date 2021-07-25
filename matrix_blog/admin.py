fffb178b0336381c935e51c47e9daa6416d57977
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-25 12:22:04 +0000

    Register models in admin.py

--- matrix_blog/admin.py
+++ matrix_blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Comment, User, Dislikes, Likes
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class LikesInline(admin.TabularInline):
+    model = Likes
+
+class DislikeInline(admin.TabularInline):
+    model = Dislikes
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body',)
+    inlines = [CommentInline, LikesInline, DislikeInline]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(User)
