4762a980224fc22fd06ef6378938233938a7d2e1
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-25 16:15:49 +0000

    Run migrations

--- matrix_blog/urls.py
+++ matrix_blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
     path('', ArticleListView.as_view(), name='article_list'),
 ]
