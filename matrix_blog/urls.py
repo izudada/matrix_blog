23efd5548e1db2a717fa0d41785e623b467da553
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-27 08:38:20 +0000

    Modify url with articlecreateview

--- matrix_blog/urls.py
+++ matrix_blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-from .views import ArticleListView, ArticleDetailView, UserCreateView
+from .views import ArticleListView, ArticleDetailView, UserCreateView, ArticleCreateView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('create_article/', ArticleCreateView.as_view(), name="create_article"),
     path('<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
     path('', ArticleListView.as_view(), name='index'),
     path('register/', UserCreateView.as_view(), name='register'),
