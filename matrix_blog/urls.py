fb3e84fd5f6b063812cf7db8114f5e4c590b0f55
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-27 04:21:52 +0000

    Add login general route and create register route

--- matrix_blog/urls.py
+++ matrix_blog/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, UserCreateView
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
-    path('', ArticleListView.as_view(), name='article_list'),
+    path('', ArticleListView.as_view(), name='index'),
+    path('register/', UserCreateView.as_view(), name='register'),
 ]
