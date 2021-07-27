348458dfd1b7e8ee2d9c00a93b7618998498c42f
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-27 08:36:07 +0000

    Change logout redirect

--- matrix_config/settings.py
+++ matrix_config/settings.py
@@ -136,5 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "index"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "login"
 LOGIN_URL = "login"
