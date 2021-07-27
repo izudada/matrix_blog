6743405f31a61575373f59454d297a1cfa2a8dea
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-27 09:45:08 +0000

    Add UpdateArticle to url, move django.contrib.auth.irls path

--- matrix_blog/urls.py
+++ matrix_blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 
-from .views import ArticleListView, ArticleDetailView, UserCreateView, ArticleCreateView
+from .views import ArticleListView, ArticleDetailView, UserCreateView, ArticleCreateView, UpdateArticle
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    path('<slug:slug>/edit/', UpdateArticle.as_view(), name="update_article"),
     path('create_article/', ArticleCreateView.as_view(), name="create_article"),
     path('<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
     path('', ArticleListView.as_view(), name='index'),
     path('register/', UserCreateView.as_view(), name='register'),
+    path('', include('django.contrib.auth.urls')),
 ]
