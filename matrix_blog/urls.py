54da1d28f8efc27cce2472cdbb3aea9251176b81
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-28 13:09:32 +0000

    Import preference view, and create preference url

--- matrix_blog/urls.py
+++ matrix_blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 
-from .views import ArticleListView, ArticleDetailView, UserCreateView, ArticleCreateView, UpdateArticle, DeleteArticleView, like_view
+from .views import ArticleListView, ArticleDetailView, UserCreateView, ArticleCreateView, UpdateArticle, DeleteArticleView, preference
 
 urlpatterns = [
-    path('<slug:slug>/like_post/', like_view, name="like_post"),
+    path('<int:pk>/preference/', preference, name="preference"),
     path('<slug:slug>/delete/', DeleteArticleView.as_view(), name="delete_article"),
     path('<slug:slug>/edit/', UpdateArticle.as_view(), name="update_article"),
     path('create_article/', ArticleCreateView.as_view(), name="create_article"),
