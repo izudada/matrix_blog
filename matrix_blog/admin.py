92909b4b2ae4ad9cef1ad7bffb94637316f42f8f
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-28 07:43:30 +0000

    Remove Likes and Dislikes from inline

--- matrix_blog/admin.py
+++ matrix_blog/admin.py
@@ -1,22 +1,15 @@
 from django.contrib import admin
 
-from .models import Article, Comment, User, Dislikes, Likes
+from .models import Article, Comment, User
 
 
 class CommentInline(admin.TabularInline):
     model = Comment
 
 
-class LikesInline(admin.TabularInline):
-    model = Likes
-
-class DislikeInline(admin.TabularInline):
-    model = Dislikes
-
-
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title',)
-    inlines = [CommentInline, LikesInline, DislikeInline]
+    inlines = [CommentInline]
 
 
 admin.site.register(Article, ArticleAdmin)
