1c1ed2de9e372d45b2f883a423a6f464b2bebea1
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-28 08:33:37 +0000

    Add url to like post

--- matrix_blog/urls.py
+++ matrix_blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 
-from .views import ArticleListView, ArticleDetailView, UserCreateView, ArticleCreateView, UpdateArticle, DeleteArticleView
+from .views import ArticleListView, ArticleDetailView, UserCreateView, ArticleCreateView, UpdateArticle, DeleteArticleView, like_view
 
 urlpatterns = [
+    path('<slug:slug>/like_post/', like_view, name="like_post"),
     path('<slug:slug>/delete/', DeleteArticleView.as_view(), name="delete_article"),
     path('<slug:slug>/edit/', UpdateArticle.as_view(), name="update_article"),
     path('create_article/', ArticleCreateView.as_view(), name="create_article"),
