86e2d2cc02e94fdb4db5c1accd4f5799ba9f2e65
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-29 21:09:15 +0000

    whitenoise to middleware

--- matrix_config/settings.py
+++ matrix_config/settings.py
@@ -57,7 +57,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
