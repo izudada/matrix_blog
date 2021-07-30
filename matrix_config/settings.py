90de10f594e881f77377f9984677759cd3abb584
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-07-30 10:48:10 +0000

    Fixing static file bugs

--- matrix_config/settings.py
+++ matrix_config/settings.py
@@ -169,4 +169,4 @@ AWS_S3_HOST = "s3.us-east-2.amazonaws.com"
 AWS_S3_REGION_NAME="us-east-2"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
